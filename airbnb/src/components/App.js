267f184fcb286a23e8ef089348a7859301f39796
Paul LeTourneau <pletourneau62@gmail.com>
2023-11-13 10:19:59 -0800

    WIP commit

---
+++ airbnb/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Listing from "./Listing";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Listing />
+    </React.Fragment>
+  );
+}
+export default App;
---
+++ airbnb/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <form>
+      <input type="text" placeholder="Location" />
+      <input type="date" />
+      <input type="text" placeholder="Add Guests" />
+    </form>
+  );
+}
+
+export default Header;
